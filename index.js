b1d387a0482588ac794985c20b28397e9a19d7ba
weidabao <wei.yucheng@foxmail.com>
2016-10-01 20:11:20 +0800

    初始化数据库课设

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//加载模板引擎ejs
+app.engine('html', require('ejs').renderFile);
+app.set('views', './');
+app.set('view engine', 'html');
+
+//绑定路由处理
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    var path = req.path;
+
+    if (path === '/favicon.ico') {
+        res.status(404).end();
+    }
+
+    if (path == "/") {
+        path = "ismartjs/index.html";
+    } else {
+        path = "ismartjs/" + path;
+    }
+
+    if (path.indexOf(".html") != -1) {
+        res.render(path);
+    } else {
+        res.sendFile(path, {root: __dirname});
+    }
+});
+
+//启动express服务器
+app.listen('3000', function () {
+    console.log('server started');
+});
