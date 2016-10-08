8a991083ab04d1c9ceea3ef4a732dbe60c5cf178
xiaobao66 <xiaobao66@bingyan.net>
2016-10-08 12:57:48 +0800

    修改超级管理员教师工作量路由管理

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var teacherClassInfo = require('./router/teacher_class_info');
 var teacherInfoManager = require('./router/teacher_info_manager');
 var userInfoManager = require('./router/user_info_manager');
 var titleInfoManager = require('./router/title_info_manager');
+var teacherWorkloadInfoManager = require('./router/teacher_workload_info_manager');
 
 var db = require('./lib/mysql').config(require('./db.config.js'));
 
@@ -74,92 +75,9 @@ app.use('/', userInfoManager.userInfoManager);
 titleInfoManager.config(db);
 app.use('/', titleInfoManager.titleInfoManager);
 
-//获取教师工作量年份
-app.get('/rest/workload_year_manager', function (req, res) {
-    var yearInfoSql = "SELECT DISTINCT\n" +
-        "	`year`\n" +
-        "FROM\n" +
-        "	job_info";
-
-    db.query(yearInfoSql).done(function (result, fields) {
-        var data = [];
-        for (var i = 0; i < result.length; i++) {
-            data[i] = {
-                id: result[i].year,
-                name: result[i].year
-            }
-        }
-        res.json(data);
-    })
-});
-
-//获取教师工作量具体信息
-app.get('/rest/workload_info_manager', function (req, res) {
-    //获取教师工作量总条数
-    var totalWorkloadSql = "SELECT\n" +
-        "	count(*) total\n" +
-        "FROM\n" +
-        "	job_info,\n" +
-        "	teacher_info\n" +
-        "WHERE\n" +
-        "	job_info.teacher_id = teacher_info.teacher_id";
-
-    //获取教师工作量详细信息
-    var workloadInfoSql = "SELECT\n" +
-        "	job_info.teacher_id,\n" +
-        "	teacher_info.`name` teacher_name,\n" +
-        "	`year`,\n" +
-        "	research,\n" +
-        "	graduation_design,\n" +
-        "	master_doctor,\n" +
-        "	score\n" +
-        "FROM\n" +
-        "	job_info,\n" +
-        "	teacher_info\n" +
-        "WHERE\n" +
-        "	job_info.teacher_id = teacher_info.teacher_id";
-
-    if (req.query.teacherId) {
-        totalWorkloadSql += ' AND job_info.teacher_id = ' + req.query.teacherId;
-        workloadInfoSql += ' AND job_info.teacher_id = ' + req.query.teacherId;
-    } else if (req.query.teacherName) {
-        totalWorkloadSql += ' AND teacher_info.`name` LIKE ' + "'%" + req.query.teacherName + "%'";
-        workloadInfoSql += ' AND teacher_info.`name` LIKE ' + "'%" + req.query.teacherName + "%'";
-    }
-
-    if (req.query.year) {
-        totalWorkloadSql += ' AND year = ' + req.query.year;
-        workloadInfoSql += ' AND year = ' + req.query.year;
-    }
-
-    totalWorkloadSql += ' ORDER BY CONVERT (`year`, SIGNED) DESC';
-    workloadInfoSql += ' ORDER BY CONVERT (`year`, SIGNED) DESC';
-    workloadInfoSql += ' LIMIT ' + (req.query.page - 1) * req.query.pageSize + ', ' + req.query.pageSize;
-
-    var data = {
-        page: req.query.page,
-        pageSize: req.query.pageSize
-    };
-
-    db.query(totalWorkloadSql).done(function (result, fields) {
-        data.total = result[0].total;
-        db.query(workloadInfoSql).done(function (result, fields) {
-            data.results = [];
-            for (var i = 0; i < result.length; i++) {
-                data.results[i] = {
-                    teacher_id: result[i]['teacher_id'],
-                    teacher_name: result[i].teacher_name,
-                    year: result[i].year,
-                    research: result[i].research,
-                    graduation_design: result[i]['graduation_design'],
-                    master_doctor: result[i]['master_doctor'],
-                    score: result[i].score
-                }
-            }
-            res.json(data);
-        })
-    })
-});
+//教师工作量路由处理
+teacherWorkloadInfoManager.config(db);
+app.use('/', teacherWorkloadInfoManager.teacherWorkloadInfoManager);
 
 //获取课程信息
 app.get('/rest/class_info_manager', function (req, res) {
