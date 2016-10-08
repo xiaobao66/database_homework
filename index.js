37592a2093c693c873b66ca94aa56686bd8def7e
xiaobao66 <xiaobao66@bingyan.net>
2016-10-08 12:12:12 +0800

    修改超级管理员用户信息路由管理

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ var teacherInfo = require('./router/teacher_info');
 var teacherWorkloadInfo = require('./router/teacher_workload_info');
 var teacherClassInfo = require('./router/teacher_class_info');
 var teacherInfoManager = require('./router/teacher_info_manager');
+var userInfoManager = require('./router/user_info_manager');
 
 var db = require('./lib/mysql').config(require('./db.config.js'));
 
@@ -60,85 +61,13 @@ teacherClassInfo.config(db);
 app.use('/', teacherClassInfo.teacherClassInfo);
 
 //超级管理员
-//教师基本信息管理
+//教师基本信息路由处理
 teacherInfoManager.config(db);
 app.use('/', teacherInfoManager.teacherInfoManager);
 
-//获取用户信息
-app.get('/rest/user_info_manager', function (req, res) {
-    var totalUserInfoSql = "SELECT\n" +
-        "	COUNT(*) total\n" +
-        "FROM\n" +
-        "	user_info,\n" +
-        "	teacher_info\n" +
-        "WHERE\n" +
-        "	user_info.teacher_id = teacher_info.teacher_id";
-
-    var userInfoSql = "SELECT\n" +
-        "	username,\n" +
-        "	`password`,\n" +
-        "	flag,\n" +
-        "	teacher_info.teacher_id,\n" +
-        "	`name`\n" +
-        "FROM\n" +
-        "	user_info,\n" +
-        "	teacher_info\n" +
-        "WHERE\n" +
-        "	user_info.teacher_id = teacher_info.teacher_id";
-
-    if (req.query.teacherId) {
-        totalUserInfoSql += ' AND user_info.teacher_id LIKE ' + "'%" + req.query.teacherId + "%'";
-        userInfoSql += ' AND user_info.teacher_id LIKE ' + "'%" + req.query.teacherId + "%'";
-    } else if (req.query.teacherName) {
-        totalUserInfoSql += ' AND teacher_info.`name` LIKE ' + "'%" + req.query.teacherName + "%'";
-        userInfoSql += ' AND teacher_info.`name` LIKE ' + "'%" + req.query.teacherName + "%'";
-    }
-
-    userInfoSql += ' LIMIT ' + (req.query.page - 1) * req.query.pageSize + ', ' + req.query.pageSize;
-
-    var data = {
-        page: req.query.page,
-        pageSize: req.query.pageSize
-    };
-
-    db.query(totalUserInfoSql).done(function (result, fields) {
-        data.total = result[0].total;
-        db.query(userInfoSql).done(function (result, fields) {
-            data.results = [];
-            for (var i = 0; i < result.length; i++) {
-                data.results[i] = {
-                    teacher_id: result[i].teacher_id,
-                    teacher_name: result[i].name,
-                    flag: result[i].flag,
-                    username: result[i].username,
-                    password: result[i].password
-                }
-            }
-            res.json(data);
-        })
-    })
-});
-
-//修改用户信息(修改密码)
-app.post('/rest/user_info_manager_edit', function (req, res) {
-    var updateUserInfoSql = "UPDATE user_info\n" +
-        "SET `password` = ?\n" +
-        "WHERE\n" +
-        "	username = ?";
-
-    db.query(updateUserInfoSql, [req.body.password, req.body.username]).done(function (result, fields, err) {
-        if (err) {
-            res.json({
-                flag: -1
-            });
-        } else {
-            res.json({
-                flag: 1,
-                userInfo: req.body
-            });
-        }
-    });
-});
+//用户信息路由处理
+userInfoManager.config(db);
+app.use('/', userInfoManager.userInfoManager);
 
 //查询职称信息
 app.get('/rest/title_info_manager', function (req, res) {
