4c1449c8221a93855ce561f52fb9ecb5a0c9cb9d
xiaobao66 <xiaobao66@bingyan.net>
2016-10-08 12:48:46 +0800

    修改超级管理员职称信息路由管理

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ var teacherWorkloadInfo = require('./router/teacher_workload_info');
 var teacherClassInfo = require('./router/teacher_class_info');
 var teacherInfoManager = require('./router/teacher_info_manager');
 var userInfoManager = require('./router/user_info_manager');
+var titleInfoManager = require('./router/title_info_manager');
 
 var db = require('./lib/mysql').config(require('./db.config.js'));
 
@@ -69,121 +70,9 @@ app.use('/', teacherInfoManager.teacherInfoManager);
 userInfoManager.config(db);
 app.use('/', userInfoManager.userInfoManager);
 
-//查询职称信息
-app.get('/rest/title_info_manager', function (req, res) {
-    var totalTitleInfoSql = "SELECT\n" +
-        "	count(*) total\n" +
-        "FROM\n" +
-        "	title_info\n" +
-        "WHERE\n" +
-        "	1 = 1";
-
-    var titleInfoSql = "SELECT\n" +
-        "	*\n" +
-        "FROM\n" +
-        "	title_info\n" +
-        "WHERE\n" +
-        "	1 = 1";
-
-    if (req.query.titleName) {
-        totalTitleInfoSql += ' AND `name` LIKE ' + "'%" + req.query.titleName + "%'";
-        titleInfoSql += ' AND `name` LIKE ' + "'%" + req.query.titleName + "%'";
-    }
-
-    titleInfoSql += ' LIMIT ' + (req.query.page - 1) * req.query.pageSize + ', ' + req.query.pageSize;
-
-    var data = {
-        page: req.query.page,
-        pageSize: req.query.pageSize
-    };
-
-    db.query(totalTitleInfoSql).done(function (result, fields, err) {
-        data.total = result[0].total;
-        db.query(titleInfoSql).done(function (result, fields, err) {
-            data.results = [];
-            for (var i = 0; i < result.length; i++) {
-                data.results[i] = {
-                    title_id: result[i].title_id,
-                    name: result[i].name,
-                    salary: result[i].salary,
-                    allowance: result[i].allowance
-                }
-            }
-
-            res.json(data);
-        })
-    });
-});
-
-//新增职称信息
-app.post('/rest/title_info_manager_add', function (req, res) {
-    var insertTitleInfoSql = "INSERT INTO title_info (`name`, salary, allowance)\n" +
-        "VALUES\n" +
-        "	(?, ?, ?)";
-
-    db.query(insertTitleInfoSql, [req.body.title_name, req.body.salary, req.body.allowance]).done(function (result, fields, err) {
-        if (err) {
-            res.json({
-                flag: -1
-            });
-        } else {
-            res.json({
-                flag: 1
-            });
-        }
-    });
-});
-
-//删除职称信息
-app.post('/rest/title_info_manager_delete', function (req, res) {
-    var deleteTitleInfoSql = "DELETE\n" +
-        "FROM\n" +
-        "	title_info\n" +
-        "WHERE\n" +
-        "	title_id IN ?";
-
-    var deleteData = JSON.parse(req.body.deleteData),
-        deleteId = [[]];
-
-    for (var i = 0, len = deleteData.length; i < len; i++) {
-        deleteId[0][i] = deleteData[i].title_id;
-    }
-
-    db.query(deleteTitleInfoSql, [deleteId]).done(function (result, fields, err) {
-        if (err) {
-            res.json({
-                flag: -1
-            });
-        } else {
-            res.json({
-                flag: 1
-            });
-        }
-    });
-});
-
-//修改职称信息
-app.post('/rest/title_info_manager_edit', function (req, res) {
-    var updateTitleInfoSql = "UPDATE title_info\n" +
-        "SET `name` = ?,\n" +
-        " salary = ?,\n" +
-        " allowance = ?\n" +
-        "WHERE\n" +
-        "	title_id = ?";
-
-    db.query(updateTitleInfoSql, [req.body.name, req.body.salary, req.body.allowance, req.body.title_id]).done(function (result, fields, err) {
-        if (err) {
-            res.json({
-                flag: -1
-            });
-        } else {
-            res.json({
-                flag: 1,
-                titleInfo: req.body
-            });
-        }
-    });
-});
+//职称信息路由处理
+titleInfoManager.config(db);
+app.use('/', titleInfoManager.titleInfoManager);
 
 //获取教师工作量年份
 app.get('/rest/workload_year_manager', function (req, res) {
